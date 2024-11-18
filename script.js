c7b37a62ca740099bc3c5f17dbf5a4bbd94c7778
Elias Garstad Grønnestad <eliasgronnestad@gmail.com>
2024-11-18 13:12:08 +0100

    update

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 let canInteract = true;
-let data
+let elev_pool = undefined;
+let is_opened = [];
+
+fetch("Elever_2024.csv")
+  .then((response) => response.arrayBuffer())
+  .then((buffer) => {
+    const decoder = new TextDecoder("iso-8859-1"); // Change to the appropriate encoding if needed
+    const text = decoder.decode(buffer);
+    const rows = text.split("\n").map((row) => row.split(";"));
+    elev_pool = rows;
+  });
+
+fetch("elev_pool.php")
+  .then((response) => response.json())
+  .then((data) => {
+  })
+  .catch((error) => console.error("Failed to fetch elev_pool:", error));
 
 function _body_onload() {
   const kalender = document.getElementById("inner_kalender");
@@ -10,7 +26,7 @@ function _body_onload() {
     kalender.appendChild(boks);
     const child_boks = document.createElement("div");
     child_boks.classList.add("child_boks");
-    child_boks.innerText = index;
+    child_boks.innerText = index + 1;
     child_boks.id = index;
     child_boks.addEventListener("click", function () {
       boks_click(index);
@@ -40,16 +56,22 @@ function boks_click(index) {
       document.getElementById("lights_off").classList.add("light_off_trans");
     }, 4500);
     setTimeout(() => {
-      pullName(index)
+      pullName(index);
     }, 6500);
     setTimeout(() => {
       canInteract = true;
-      console.log(canInteract)
+      console.log(canInteract);
     }, 10000);
   }
 }
 
-
 function pullName(dateIndex) {
-  
+  if (elev_pool) {
+    const pull = Math.floor(Math.random() * elev_pool.length) + 1;
+    document.getElementById("winner").innerText = elev_pool[pull][0] + " " +
+      elev_pool[pull][1] + " " + elev_pool[pull][2];
+    console.log(elev_pool[40]);
+    elev_pool.splice(pull, 1);
+    is_opened.push(dateIndex);
+  }
 }
